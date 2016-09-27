c42016c83ce445c31acb7cdc04c6a7a98a41ebce
Pete <peternatewood@gmail.com>
2016-09-26 21:27:07 -0400

    Add js file with ready function

---
+++ game.js
@@ -0,0 +1,15 @@
+var ready = function(fun) {
+  if (document.readyState != 'loading') {
+    fun();
+  }
+  else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+  else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState != 'loading') {
+        fun();
+      }
+    });
+  }
+}
