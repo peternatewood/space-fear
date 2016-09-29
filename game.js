15c69c33ccb5b17d43ae63c4fd69624bcb97bc5e
Pete <peternatewood@gmail.com>
2016-09-28 22:37:09 -0400

    Add animation loop to main game script

--- game.js
+++ game.js
@@ -18,6 +18,15 @@ ready(function() {
   var canvas = document.getElementById('main-viewport');
   var context = canvas.getContext('2d');
 
-  window.keyboard = new Keyboard(canvas);
-  keyboard.render();
+  var keyboard = new Keyboard(canvas);
+
+  var step = function(timestamp) {
+    if (! start) var start = timestamp;
+    var progress = timestamp - start;
+    keyboard.render();
+    if (progress < 2000) {
+      window.requestAnimationFrame(step);
+    }
+  }
+  window.requestAnimationFrame(step);
 });
