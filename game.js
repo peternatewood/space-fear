92124726c85b2b6c68277bae7e72889b3706ac88
Pete <peternatewood@gmail.com>
2016-09-30 20:23:57 -0400

    Move keyboard listeners to main game script

--- game.js
+++ game.js
@@ -20,6 +20,35 @@ ready(function() {
 
   var keyboard = new Keyboard(canvas);
 
+  document.addEventListener('keydown', function(event) {
+    var index = convertKeyToIndex(event.key.toUpperCase());
+    if (! event.repeat && index !== false) {
+      // Don't prevent default if ctrl or alt are held down, to allow keyboard
+      // shortcuts
+      if (! event.ctrlKey && ! event.altKey) event.preventDefault();
+
+      if (index.length > 1)
+        index.forEach(function(i) {
+          keyboard.keys[i].pressed = true;
+        }, this);
+      else {
+        keyboard.keys[index].pressed = true;
+      }
+    }
+  });
+  document.addEventListener('keyup', function(event) {
+    var index = convertKeyToIndex(event.key.toUpperCase());
+    if (index !== false) {
+      if (index.length > 1)
+        index.forEach(function(i) {
+          keyboard.keys[i].pressed = false;
+        }, this);
+      else {
+        keyboard.keys[index].pressed = false;
+      }
+    }
+  });
+
   var step = function(timestamp) {
     if (! start) var start = timestamp;
     var progress = timestamp - start;
